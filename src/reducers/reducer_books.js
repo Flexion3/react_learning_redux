7acd5c92d7ce2707d8feaa1883d209a83d3a2f0d
JonasCrauwels2 <jonas.crauwels@jstack.eu>
2017-06-16 21:38:58 +0200

    reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by crauwjo on 15/06/2017.
+ */
+export default function(){
+    return [
+        {title: 'Javascript - the good parts'},
+        {title: 'Harry Potter'},
+        {title: 'Lord of the Rings'},
+        {title: 'Test book'},
+        {title: 'A very good book'}
+    ]
+}
